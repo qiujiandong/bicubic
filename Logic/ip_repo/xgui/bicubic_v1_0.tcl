f5f64929759cfbb425d16eaa6ec4ed90d7425d65
qiujiandong <qiujiandong@minieye.cc>
2024-04-25 00:26:51 +0800

    📝docs: update README.md

--- Logic/ip_repo/xgui/bicubic_v1_0.tcl
+++ Logic/ip_repo/xgui/bicubic_v1_0.tcl
@@ -3,7 +3,6 @@ proc init_gui { IPINST } {
   ipgui::add_param $IPINST -name "Component_Name"
   #Adding Page
   set Page_0 [ipgui::add_page $IPINST -name "Page 0"]
-  ipgui::add_param $IPINST -name "FRACTION_BITS" -parent ${Page_0}
   ipgui::add_param $IPINST -name "IRQ_CYCLES" -parent ${Page_0}
 
 
